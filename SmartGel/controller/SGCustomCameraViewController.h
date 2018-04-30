dae89799aba5eca64f563a4c98ac7300e49732d0
jordi albers <jordialbers81@gmail.com>
2018-04-30 10:14:04 -0700

    added AVCamCaptureManager, SGCustomCameraViewController

---
+++ SmartGel/controller/SGCustomCameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  SGCustomCameraViewController.h
+//  SmartGel
+//
+//  Created by jordi on 30/04/2018.
+//  Copyright © 2018 AFCO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGCustomCameraViewController : UIViewController
+
+@end
---
+++ SmartGel/controller/SGCustomCameraViewController.m
@@ -0,0 +1,25 @@
+//
+//  SGCustomCameraViewController.m
+//  SmartGel
+//
+//  Created by jordi on 30/04/2018.
+//  Copyright © 2018 AFCO. All rights reserved.
+//
+
+#import "SGCustomCameraViewController.h"
+
+@interface SGCustomCameraViewController ()
+
+@end
+
+@implementation SGCustomCameraViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
