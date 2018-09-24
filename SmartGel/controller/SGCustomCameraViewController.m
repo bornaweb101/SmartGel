e215ed008189693db76e76c9371d2b25616ce77a
jordi albers <jordialbers81@gmail.com>
2018-09-24 05:06:04 +0300

    updated auto detection engine

--- SmartGel/controller/SGCustomCameraViewController.m
+++ SmartGel/controller/SGCustomCameraViewController.m
@@ -198,7 +198,7 @@
                     double resultvalue = [self calculateResultValue:self.capturedImage];
                     self.statusLabel.text = [NSString stringWithFormat:@"%.2f",resultvalue];
 
-                    if(detectedCount>5){
+                    if(detectedCount>30){
                         [[self.captureManager session] stopRunning];
                         UIImageWriteToSavedPhotosAlbum(self.capturedImage,nil,nil,nil);
                         if(self.delegate){
